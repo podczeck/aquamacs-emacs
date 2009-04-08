f280dec777e1afa9918e382af4ccaa059c87af48
David Reitter <david.reitter@gmail.com>
2009-04-08 18:10:42 -0400

    modified build script to work with Git repository. Patches are no longer applied (assumed to be contained in branch). No more CVS updating. No more Emacs building.

--- aquamacs/build/build.sh
+++ aquamacs/build/build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash -l
-export CVS_RSH=ssh
 
-AQEMACSSRC='emacs.raw'
 SHORTDATE=`date +"%Y-%b-%d-%a"`
 DATE=`date +"%Y-%b-%d-%a-%H%M"`
 
-if test "$AQUAMACS_CVS_PREFIX" ; then
-    CVS_PREFIX=$AQUAMACS_CVS_PREFIX
-else
-    CVS_PREFIX="cvs -z3 -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs"
-fi
 
 AQ_PREFIX=`pwd`
 
@@ -25,7 +18,7 @@ do
 	    ;;
 	c)	COPY='copy-to-server'
 	    ;;
-	?)	printf "Usage:  %s [-l] [-c] {cvs|emacs|aquamacs|plugins} [<build-parameters>]\nExpects directories ./aquamacs and ./emacs.raw (by default),\nso call from top-level directory.\n\n" $(basename $0) >&2
+	?)	printf "Usage:  %s [-l] [-c] {aquamacs|plugins} [<build-parameters>]\nExpects directories ./aquamacs and ./emacs.raw (by default),\nso call from top-level directory.\n\n" $(basename $0) >&2
 	    exit 2
 	    ;;
     esac
@@ -33,7 +26,7 @@ done
 shift $(($OPTIND - 1))
 a='aquamacs'
 case $1 in 
-    aquamacs | emacs | plugins | cvs) 
+    aquamacs | plugins) 
 	a=$1
 	shift $((1))
 esac	
@@ -49,20 +42,10 @@ fi
 
 
     
-if test "$a" == "emacs" ; then
-    BUILD_GNU_EMACS=yes  
-    if [ $LOGPAR ]; then
-	LOG=${AQ_PREFIX}/emacs-build.log
-    fi
-elif test "$a" == "aquamacs" ; then
+if  test "$a" == "aquamacs" ; then
     BUILD_AQUAMACS=yes  
     if [ $LOGPAR ]; then
 	LOG=${AQ_PREFIX}/aquamacs-build.log
-    fi
-elif test "$a" == "cvs" ; then
-    UPDATE_CVS=yes  
-    if [ $LOGPAR ]; then
-	LOG=${AQ_PREFIX}/cvs-update.log
     fi
 elif test "$a" == "plugins" ; then
     UPDATE_PLUGINS=yes  
@@ -86,14 +69,6 @@ date >${LOG}
 
 cd ${AQ_PREFIX}
 
-if test "${UPDATE_CVS}" == "yes"; then
-
-
-    cd emacs.raw
-    echo "CVS update: emacs" >>$LOG  
-    CVS_RSH=ssh cvs update -dP >>$LOG 2>>$LOG
- 
-fi
 
 if test "${UPDATE_PLUGINS}" == "yes"; then
     rm -rf ${DEST}/plugins
@@ -119,67 +94,16 @@ if test "${UPDATE_PLUGINS}" == "yes"; then
     fi
 
 fi
-if test "${BUILD_GNU_EMACS}" == "yes"; then
-    
-
-    rm -rf emacs.GNU 2>>$LOG
-    echo "Copying emacs.raw emacs.GNU" >>$LOG  
-    cp -Rp emacs.raw emacs.GNU  2>>$LOG 
-
-    if [ $? != 0 ]; then
-	exit 1
-    fi
-
-    export EMACS_ROOT=`pwd`/emacs.GNU
-    cd emacs.GNU/mac
-
-    export MACOSX_DEPLOYMENT_TARGET=10.4
-   
-    echo "Building Emacs (make-package)..." >>$LOG 
-    ./make-package --self-contained --build-in-place >>$LOG 2>>$LOG 
-
-    NAME=GNU-Emacs-$DATE
-
-    mv EmacsInstaller.dmg ${NAME}.dmg
-    bzip2 ${NAME}.dmg  >>$LOG 2>>$LOG 
-    rm -rf ${DEST}/GNU-Emacs*
-    mv ${NAME}.dmg.bz2 ${DEST}/
-
-    echo "If succeeded, result in " ${NAME}.dmg  >>$LOG  
-    date >>${LOG}
-
-fi
 
 
 if test "${BUILD_AQUAMACS}" == "yes"; then
      
-    rm -rf emacs  2>>$LOG 
-    echo "Copying $AQEMACSSRC to emacs" >>$LOG  
-    cp -Rp $AQEMACSSRC emacs  2>>$LOG 
-
     if [ $? != 0 ]; then
 	exit 1
     fi
 
-    export EMACS_ROOT=`pwd`/emacs
+    export EMACS_ROOT=`pwd`
     cd aquamacs
-    echo "CVS update: aquamacs" >>$LOG  
-    $CVS_PREFIX update -dP >>$LOG 2>>$LOG 
-
-    echo "Downloading online patches" >>$LOG
-
-#     curl 'http://svn.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/zenitani/CarbonEmacs/src/patch/mac-functions.patch?root=macwiki' -o ${AQUAMACS_ROOT}/patches/mac-functions.patch 2>>$LOG
-#     curl 'http://svn.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/zenitani/CarbonEmacs/src/patch/transparency4.patch?root=macwiki' -o ${AQUAMACS_ROOT}/patches/transparency.z.patch 2>>$LOG
-#     curl 'http://svn.sourceforge.jp/cgi-bin/viewcvs.cgi/*checkout*/zenitani/CarbonEmacs/src/patch/emacs-inline.patch?root=macwiki' -o ${AQUAMACS_ROOT}/patches/emacs-inline.z.patch 2>>$LOG
-
-#     sed -e 's/^--- emacs.orig\//--- /g' -e 's/+++ emacs\//+++ /g'  ${AQUAMACS_ROOT}/patches/emacs-inline.z.patch >${AQUAMACS_ROOT}/patches/emacs-inline.patch
-#     sed -e 's/^--- emacs.orig\//--- /g' -e 's/+++ emacs\//+++ /g'  ${AQUAMACS_ROOT}/patches/transparency.z.patch >${AQUAMACS_ROOT}/patches/transparency.patch
-
-
-    echo "Applying Aquamacs patches..." >>$LOG  
-    . ${AQUAMACS_ROOT}/build/apply-patches.sh >>$LOG 2>>$LOG
-
-    
 
     # -g -> debug symbols
     # -O3 -> max optimize (speed)
