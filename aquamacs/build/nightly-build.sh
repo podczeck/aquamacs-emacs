b6df65bf96f2a3b9c389538bb915e6dd6538a52b
David Reitter <david.reitter@gmail.com>
2009-05-02 04:21:50 +0000

    force re-build (make clean)

--- aquamacs/build/nightly-build.sh
+++ aquamacs/build/nightly-build.sh
@@ -6,6 +6,7 @@ cd aquamacs-emacs.git
 rm aquamacs-build.log
 git checkout -f Aquamacs22 >>aquamacs-build.log
 git pull origin Aquamacs22 >>aquamacs-build.log
+make clean  # ensure build actually takes place
 ./build-aquamacs.sh -lc aquamacs
 
 
