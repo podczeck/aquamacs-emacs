06556f0dfa66735784fff3609c66c93e6c3b2c03
David Reitter <david.reitter@gmail.com>
2010-05-25 21:35:43 -0400

    add "reopen" (ns-application-reopen) event

--- src/nsterm.m
+++ src/nsterm.m
@@ -4550,7 +4550,7 @@ ns_term_shutdown (int sig)
   if (!emacs_event)
     return;
   emacs_event->kind = NS_NONKEY_EVENT;
-  emacs_event->code = KEY_NS_APPLICATION_ACTIVATED;
+  emacs_event->code = KEY_NS_APPLICATION_REOPEN;
   EV_TRAILER ((id)nil);
 
   return NO;
